57c761b3f267c15f1b73293fb52ceb85192053d7
DP <rdivyaprakash78@gmail.com>
2025-02-27 17:06:00 +0000

    env file added

--- db.py
+++ db.py
@@ -1,9 +1,18 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://dprakashdprakash32:Ramayanam1!@nvd-cve.17zcq.mongodb.net/?retryWrites=true&w=majority&appName=NVD-CVE"
+load_dotenv()
+
+username = str(os.environ.get('db_username'))
+password = str(os.environ.get('db_password'))
+
+uri = f"mongodb+srv://{username}:{password}@nvd-cve.17zcq.mongodb.net/?retryWrites=true&w=majority&appName=NVD-CVE"
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 db = client.vulnerabilities
 
 cves = db["cve_info"]
+
+
